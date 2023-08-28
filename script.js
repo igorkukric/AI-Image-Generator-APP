d0ecca0bc629b82e4e3f7a27172c5bdb5d1c6cbd
igorkukric <you@example.com>
2023-08-29 00:14:21 +0200

    add Function to generate random number between min and max (inclusive) add Function to disable the generate button during proccesing add Function to enagle the generate button after procces add Function to clear image grid

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-const apiKey = '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'
+const apiKey = "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";
+
+const maxImages = 4; // Number of images to generate for each prompt
+let selectedImageNumber = null;
+
+// Function to generate random number between min and max (inclusive)
+function getRandomNumber(min, max) {
+  return Math.random(Math.random() * (max - min + 1)) + min;
+}
+
+// Function to disable the generate button during proccesing
+function disableGenerateButtton() {
+  document.getElementById("generate").disabled = true;
+}
+// Function to enagle the generate button after procces
+function disableGenerateButtton() {
+  document.getElementById("generate").disabled = false;
+}
+
+// Function to clear image grid
+function clearImageGrid() {
+  const imageGrid = document.getElementById("image-grid");
+  imageGrid.innerHTML = "";
+}
